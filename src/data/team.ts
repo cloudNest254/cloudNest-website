04bafaab527a19858e7f02909490f060dc19cd0c
omuse10 <gomuse65@gmail.com>
2025-07-30 09:43:45 +0300

    pic add

--- src/data/team.ts
+++ src/data/team.ts
@@ -4,7 +4,7 @@ export const team: TeamMember[] = [
   {
     id: 1,
     name: 'Geoffrey Omuse',
-    role: 'Creative Director & Digital Marketing Specialist',
+    role: 'Software Developer, Creative Director & UI/UX Designer',
     email: 'gomuse65@gmail.com',
     phone: '0743402487',
     image: 'https://i.postimg.cc/c4HQ0rpy/omuse.jpg',
@@ -13,10 +13,28 @@ export const team: TeamMember[] = [
   {
     id: 2,
     name: 'Tyra Nyambura',
-    role: 'Cloud Engineer',
+    role: 'Cloud Engineer, Cloud Infrastructure Specialist and Kubernetes Administrator',
     email: 'nyamburatyra453@gmail.com',
     phone: '0745811338',
     image: 'https://i.postimg.cc/pLL1NWZp/tyra.jpg',
     division: 'cloud'
+  },
+  {
+    id: 3,
+    name: 'Kiptum Victor',
+    role: 'Software Engineer, DevOps Specialist, CI/CD Expert, Cloud Automation Engineer',
+    email: 'kiptumvictor@gmail.com',
+    phone: '+254732572192',
+    image: 'https://i.postimg.cc/RFqn3LBD/vic.jpg',
+    division: 'cloud'
+  },
+  {
+    id: 4,
+    name: 'Myra ayiera',
+    role: 'Software Developer, creative strategist & Digital Marketing Specialist',
+    email: 'ayieramyra@gmail.com',
+    phone: '+254 740 958201',
+    image: 'https://i.postimg.cc/VvCrgs59/myra.jpg',
+    division: 'creative'
   }
 ];
